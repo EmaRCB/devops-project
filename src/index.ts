02137f65c516db3b60b9872b84cbc2284fce441d
Victor Mendoza <vmendoza2002@hotmail.com>
2024-04-06 00:44:29 -0600

    add (vehiculos): se añadio el router de vehiculos y controlador

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { app } from "./app";
 import { client } from "./util/database_client";
 import { EnvLoad } from "./util/env_load";
 
+const vehiculosRouter = require('./f_crud_vehiculos/vehiculos.router');
 app.listen(EnvLoad.PORT, async () => {
   console.info(`Server running on http://localhost:${EnvLoad.PORT}`);
-  client;
 });
+
+app.use('/vehiculos', vehiculosRouter);
