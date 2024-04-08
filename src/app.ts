26baaf470e9e94f5a2e08b889349320562cebf3b
Jamart Peres <alt.damon.j@gmail.com>
2024-04-07 19:44:08 -0600

    feat(admistradores): CRUD para conductores completo

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import Express from "express";
 import cors from "cors";
+import ConductorVista from "../src/f_crud_conductores_02/Vista/ConductorVista"; // Asegúrate de que la ruta de importación sea correcta
 
 export const app = Express();
 
 app.use(Express.urlencoded({ extended: true }));
 app.use(Express.json());
 app.use(cors());
+
+// Montar las rutas de conductores en un endpoint específico, por ejemplo '/api/conductores'
+app.use('/api/conductores', ConductorVista);
+
