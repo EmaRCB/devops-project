8beb61fd380c83c7bfb4f4088e6167fb11393bd7
Victor Mendoza <vmendoza2002@hotmail.com>
2024-04-07 21:07:23 -0600

    add (vehiculos): se añadio el CRUD de vehiculos

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import { app } from "./app";
 import { client } from "./util/database_client";
 import { EnvLoad } from "./util/env_load";
 
-const vehiculosRouter = require('./f_crud_vehiculos/vehiculos.router');
+const vehiculosRouter = require('./f_crud_vehiculos/vehiculo.router');
 app.listen(EnvLoad.PORT, async () => {
   console.info(`Server running on http://localhost:${EnvLoad.PORT}`);
+  client;
 });
 
 app.use('/vehiculos', vehiculosRouter);
