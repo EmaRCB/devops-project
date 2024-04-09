4038ca96ffa1e004c852ddc28d5f5c21f5fb2a30
Jamart Peres <alt.damon.j@gmail.com>
2024-04-08 20:11:53 -0600

    fix(admistradores): cambio en la ruta en app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import Express from "express";
 import cors from "cors";
-import ConductorVista from "../src/f_crud_conductores_02/Vista/ConductorVista"; // Asegúrate de que la ruta de importación sea correcta
+import ConductorVista from "../src/f_crud_conductores_02/Vista/ConductorVista"; 
 
 export const app = Express();
 
@@ -8,6 +8,6 @@ app.use(Express.urlencoded({ extended: true }));
 app.use(Express.json());
 app.use(cors());
 
-// Montar las rutas de conductores en un endpoint específico, por ejemplo '/api/conductores'
-app.use('/api/conductores', ConductorVista);
+// Ruta para crud de conductores
+app.use('/conductores', ConductorVista);
 
