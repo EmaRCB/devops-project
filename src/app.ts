ecc0f54dad760ed2547cc46106dc8559639e1c82
Victor Mendoza <vmendoza2002@hotmail.com>
2024-04-09 20:13:15 -0600

    hotfix(rutas): modificacion de los endpoints de rutas

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const adminRutas = require('./f_registro_administradores_04/vista/AdminRutas');
 app.use(Express.urlencoded({ extended: true }));
 app.use(Express.json());
 app.use(cors());
-app.use('/api/rutas', RutasVista);
+app.use('/rutas', RutasVista);
 app.use('/admin', adminRutas);
 app.use('/conductores', ConductorVista);
 app.use("/asignaciones", asisgnacionesRouter);
